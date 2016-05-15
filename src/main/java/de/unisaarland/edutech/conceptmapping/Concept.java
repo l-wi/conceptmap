ccfb1478b3853cb26d84f6b5342011eaa92f1318
Tim Steuer <steuertim@gmail.com>
2016-05-15 14:20:43 +0200

    changes for voting component

--- src/main/java/de/unisaarland/edutech/conceptmapping/Concept.java
+++ src/main/java/de/unisaarland/edutech/conceptmapping/Concept.java
@@ -1,13 +1,16 @@
 package de.unisaarland.edutech.conceptmapping;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Concept implements Serializable, Cloneable{
+public class Concept implements Serializable, Cloneable {
 
 	private CollaborativeString name;
 	private double x;
 	private double y;
 	private double rotate;
+	private Set<User> votes = new HashSet<>();
 
 	public Concept(CollaborativeString name) {
 		this.name = name;
@@ -34,12 +37,12 @@ public class Concept implements Serializable, Cloneable{
 		return y;
 	}
 
-	public void setPosition(double x, double y,double r){
+	public void setPosition(double x, double y, double r) {
 		setX(x);
 		setY(y);
 		setRotate(r);
 	}
-	
+
 	private void setX(double x) {
 		if (x < 0 || x > 1)
 			throw new RuntimeException("x coordinate for concept does not fullfill 0<x<1 x=" + x);
@@ -55,7 +58,7 @@ public class Concept implements Serializable, Cloneable{
 	private void setRotate(double rotate) {
 		this.rotate = rotate;
 	}
-	
+
 	public double getRotate() {
 		return rotate;
 	}
@@ -63,7 +66,7 @@ public class Concept implements Serializable, Cloneable{
 	@Override
 	public Concept clone() {
 		try {
-			Concept c =  (Concept) super.clone();
+			Concept c = (Concept) super.clone();
 			c.name = name.clone();
 			return c;
 		} catch (CloneNotSupportedException e) {
@@ -71,7 +74,19 @@ public class Concept implements Serializable, Cloneable{
 		}
 	}
 
+	public int getVotes() {
+		return votes.size();
+	}
 
-	
-	
+	public void setVoted(User user, boolean b) {
+		if (b)
+			votes.add(user);
+		else
+			votes.remove(user);
+	}
+
+	public boolean hasVoted(User u) {
+		return votes.contains(u);
+	}
+
 }
